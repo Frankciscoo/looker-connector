8e607e61eed513effc82aac68d1286930973f6ac
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-16 14:36:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,8 +132,7 @@ else:
                 exclude_filters_assignment.append(index)
 
     # Handle user input and configuration
-    st.markdown("*Do you have a configuration file to upload?*")
-    config_file = st.radio("", ('Yes', 'No'))
+    config_file = st.radio("**Do you have a configuration file to upload?**", ('Yes', 'No'))
 
     if config_file == 'Yes':
         uploaded_file = st.file_uploader("Upload a document (.txt)", type=("txt"))
