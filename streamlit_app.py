785165eada27fabb033afd6fa302965684dec3af
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-16 11:27:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,8 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 client_id = st.text_input("Looker API Key", type="password")
-if not openai_api_key:
+if not client_id:
     st.info("Please add your Looker API keys to continue. (Client ID & Secret)", icon="🗝️")
 else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    
