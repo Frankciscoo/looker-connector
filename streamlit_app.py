94b5f19080073280ecac0823ae08fc1cdb1745c0
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-16 14:38:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.title("🔗 Looker-Gsheets Connector")
 st.write("XXX Write explanation")
 
 # Ask user for their Looker API client_id and client_secret via `st.text_input`
-client_id = st.text_input("Looker API Client ID", type="password")
-client_secret = st.text_input("Looker API Client Secret", type="password")
+client_id = st.text_input("**Looker API Client ID**", type="password")
+client_secret = st.text_input("**Looker API Client Secret**", type="password")
 
 if not client_id or not client_secret:
     st.info("Please add your Looker API Client ID and Client Secret to continue.", icon="🗝️")
