817aed606700def8ce16eefc8dac5d0045ba5218
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-16 15:57:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import  streamlit   as st
+import  requests
+import  pandas            as pd
+import  numpy             as np
+import  gspread
+import  json
+from    google.colab              import auth, drive, userdata
+from    google.auth               import default
+from    gspread_dataframe         import get_as_dataframe, set_with_dataframe
+from    googleapiclient.discovery import build
 
 # Initial declarations
 number_of_looks = None
