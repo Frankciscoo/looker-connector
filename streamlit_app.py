59fde1ab6f2f5e12d1d1d0d5b6e7a19f2cab444d
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-19 16:20:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -363,4 +363,4 @@ else:
     
     # Create a DataFrame from looks_list
     looks = pd.DataFrame(np.array(looks_list).reshape(-1, 1), columns=['look_id'])
-    st.write(looks)
+    st.write(number_of_looks)
