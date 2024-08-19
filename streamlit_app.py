f890bf71f653b0cc15cd21c94e83c6e2cca8b32b
Frankciscoo <56899330+Frankciscoo@users.noreply.github.com>
2024-08-19 14:44:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,8 +265,8 @@ else:
                     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                     "token_uri": "https://oauth2.googleapis.com/token",
                     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                    "project_id": st.secrets["google"]["project_id"]
-                    # Notice: No "redirect_uris" key here
+                    "project_id": st.secrets["google"]["project_id"],
+                    "redirect_uris": "http://localhost:8501/"
                 }
             }
     
